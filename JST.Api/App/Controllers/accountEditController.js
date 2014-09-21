e45413f905dc842b8f70ffc64662ce72468f9f8e
Dan Thomas <danthomas000@gmail.com>
2014-09-21 13:05:51 +0100

    commi

---
+++ JST.Api/App/Controllers/accountEditController.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+app.controller('accountEditController', function ($scope, $routeParams, $location, globalData, accountService) {
+    globalData.pageName = 'Accounts';
+
+    $scope.selectedAccounts = [];
+
+    if ($routeParams.accountId) {
+        accountService.getAccountEdit($routeParams.accountId, function(data) {
+            $scope.account = data.model;
+        });
+    } else {
+        $scope.account = {
+            accountId: 0,
+            accountName: '',
+            displayName: ''
+        };
+    }
+
+    $scope.save = function () {
+        accountService.saveAccount($scope.account, function (data) {
+            if (data.isSuccess) {
+                $location.url('/accountList');
+            } else {
+                
+            }
+        });
+    };
+
+    $scope.cancel = function () {
+        $location.url('/accountList');
+    };
+});
---
+++ JST.Api/App/Controllers/accountListController.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+app.controller('accountListController', function ($scope, $location, globalData, accountService) {
+    globalData.pageName = 'Accounts';
+
+    $scope.selectedAccounts = [];
+
+    accountService.getAccountList(function (data) {
+        $scope.accounts = data.model;
+    });
+
+    $scope.rowClick = function (account) {
+        $scope.selectedAccounts = [account];
+    };
+
+    $scope.add = function () {
+        $location.url('/accountEdit');
+    };
+
+    $scope.edit = function (account) {
+        $location.url('/accountEdit/' + account.accountId);
+    };
+});
