da1b23324605a07b5f04a5419369a82176fe1b77
Dan Thomas <danthomas000@gmail.com>
2014-09-23 13:46:18 +0100

    default page

--- JST.Api/Publish.bat
+++
@@ -1,5 +0,0 @@
-﻿option batch abort
-option confirm off
-open ftp://user:password@example.com/
-synchronize remote "C:\Users\Dan\Documents\Visual Studio 2013\Projects\DTS.AppFramework\Solutions\JST\JST.Api\*.*" /danthoma/jst.dan-thomas.net/wwwroot
-close
